1caae9e4a6e73216789d71e8a1d5e01078e401c8
Scott Simpson <scott.simpson@avanade.com>
2022-08-10 08:45:25 -0700

    add title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.scss';
-import React from 'react';
-import { Button } from 'reactstrap';
+import "./App.scss";
+import React from "react";
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-        <Button color="danger">Danger!</Button>
-
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Welcome to SongFlow</h3>
       </header>
     </div>
   );
