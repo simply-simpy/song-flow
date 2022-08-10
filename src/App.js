b9be3b1ce666e6ea308684e88c4fa61a673da37b
Scott Simpson <scott.simpson@avanade.com>
2022-08-10 08:54:49 -0700

    update title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Welcome to SongFlow</h3>
+        <h3>Welcome to SongFlow!</h3>
       </header>
     </div>
   );
